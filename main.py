ee6d2bace8bf7d93e67a3623ee5f32f17e800993
Joshua Yap <joshuayap98@gmail.com>
2020-05-29 17:34:06 +0800

    Changed Project structure of Scrapy module
    Change:
    - Added run_scraper.py in postscrawl folder to manage AAPL_posts_spider
    - Added main.py at root
    - Added DEBUG feature (change settings.json -> python.pythonPath: ${workspaceFolder}/bin/python). Ref: https://stackoverflow.com/questions/54009081/how-can-i-debug-a-python-code-in-a-virtual-environment-using-vscode

---
+++ main.py
@@ -0,0 +1,9 @@
+from postscrawl.run_scraper import Scraper
+
+def main():
+   scraper = Scraper()
+   scraper.start_AAPL_posts_crawl()
+
+
+if __name__ == '__main__':
+    main()
---
+++ postscrawl/run_scraper.py
@@ -0,0 +1,16 @@
+import scrapy
+import os
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from postscrawl.postscrawl.spiders.AAPL_posts_spider import AAPLPostSpider
+
+class Scraper(object):
+    def __init__(self):
+        settings_file_path = 'postscrawl.postscrawl.settings'
+        os.environ.setdefault('SCRAPY_SETTINGS_MODULE', settings_file_path)
+        self.process = CrawlerProcess(get_project_settings())
+        self.spider = AAPLPostSpider
+
+    def start_AAPL_posts_crawl(self):
+        self.process.crawl(self.spider)
+        self.process.start()

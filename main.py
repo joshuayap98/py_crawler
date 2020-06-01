6701a4c660ef367dde19469b9fdf2f22b21e2167
Joshua Yap <joshuayap98@gmail.com>
2020-06-01 16:21:06 +0800

    Classification-Model (BOW) update
    - Added dataset 'combined_news_DJIA' from https://www.kaggle.com/aaron7sun/stocknews
    - Added sample implementation code from: https://www.kaggle.com/ndrewgele/omg-nlp-with-the-djia-and-reddit

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from postscrawl.run_scraper import Scraper
 from postscrawl.classifier.classifier import Classifier
-def main():
-   scraper = Scraper()
-   scraper.start_AAPL_posts_crawl()
 
+def main():
+   # scraper = Scraper()
+   # scraper.start_AAPL_posts_crawl()
+   classifier=Classifier()
+   dataset=classifier.read_csv_data('./postscrawl/classifier/data/Combined_News_DJIA.csv')
+   train_dataset = dataset[dataset['Date'] < '2015-01-01']
+   test_dataset = dataset[dataset['Date'] > '2014-12-31']
+   data_model=classifier.train_model(train_dataset)
+   classifier.prediction_result(test_dataset, data_model)
 
 if __name__ == '__main__':
     main()
--- postscrawl/classifier/classifier.py
+++ postscrawl/classifier/classifier.py
@@ -1,14 +1,23 @@
 import nltk
-from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import sent_tokenize, word_tokenize, RegexpTokenizer
 from nltk.probability import FreqDist
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+
 import pandas as pd
+
 
 class Classifier(object):
     def __init__(self):
         self.lem = WordNetLemmatizer()
+        # ngram_range=(1,1) Bag of words (default) 
+        # ngram_range=(2,2) Bigrams only 
+        # ngram_range=(1,2) Both!
+        self.basicvectorizer = CountVectorizer(ngram_range=(1,2))
+        self.basicmodel = LogisticRegression(max_iter=1000)
 
     def sent_tokenisation(self, sent):
         return sent_tokenize(sent)
@@ -33,3 +42,46 @@ class Classifier(object):
 
     def pos_tagging(self, tokenized_word_array):
         return nltk.pos_tag(tokenized_word_array)
+
+    def read_csv_data(self, path):
+        return pd.read_csv(path)
+
+    def visualise_csv_data(self, tokenized_word_array):
+        print(pd.DataFrame([[x,tokenized_word_array.count(x)] for x in set(tokenized_word_array)], 
+                columns = ['Word', 'Count']))
+    """
+    [summary] generates a document term matrix to count the number of occurance of a single word
+    """
+    def generate_term_matrix(self, data):
+        token = RegexpTokenizer(r'[a-zA-Z0-9]+')
+        cv = CountVectorizer(lowercase=True,stop_words='english',ngram_range = (1,1),tokenizer = token.tokenize)
+        text_count = cv.fit_transform(data)
+        return text_count
+
+    def get_train_headlines(self, train_dataset):
+        trainheadlines = []
+        # dataset.iloc[...] does not include headers
+        for row in range(0,len(train_dataset.index)):
+            trainheadlines.append(' '.join(str(x) for x in train_dataset.iloc[row,2:27]))
+        return trainheadlines
+
+    def train_model(self, train_dataset):
+        basictrain = self.basicvectorizer.fit_transform(self.get_train_headlines(train_dataset))
+        self.get_train_headlines(train_dataset)
+        # model training
+        return self.basicmodel.fit(basictrain, train_dataset["Label"])
+
+    def prediction_result(self, test_dataset, basic_model):
+        testheadlines = []
+        for row in range(0,len(test_dataset.index)):
+            testheadlines.append(' '.join(str(x) for x in test_dataset.iloc[row,2:27]))
+        basictest = self.basicvectorizer.transform(testheadlines)
+        predictions = basic_model.predict(basictest)
+        print(pd.crosstab(test_dataset["Label"], predictions, rownames=["Actual"], colnames=["Predicted"]))
+        
+        basicwords = self.basicvectorizer.get_feature_names()
+        basiccoeffs = basic_model.coef_.tolist()[0]
+        coeffdf = pd.DataFrame({'Word' : basicwords, 
+                                'Coefficient' : basiccoeffs})
+        coeffdf = coeffdf.sort_values(['Coefficient', 'Word'], ascending=[0, 1])
+        print(coeffdf.head(10))
